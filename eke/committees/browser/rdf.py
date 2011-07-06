f149479208feefe6293a2b5b87777dd0885d9a56
Sean Kelly <sean.c.kelly@me.com>
2011-07-06 04:46:30 +0000

    Re-attach CG members to CG objects after committee feed ingest

--- eke/committees/browser/rdf.py
+++ eke/committees/browser/rdf.py
@@ -6,14 +6,32 @@
 EKE Committees: RDF ingest for Committee Folders and their Committees.
 '''
 
+from Acquisition import aq_inner
 from eke.knowledge.browser.rdf import KnowledgeFolderIngestor, CreatedObject, registerHandler, IngestHandler
 from eke.knowledge.browser.utils import updateObject
+from Products.CMFCore.utils import getToolByName
 from rdflib import URIRef
 
 _committeeTypeURI = URIRef('http://edrn.nci.nih.gov/rdf/types.rdf#Committee')
+_collabGroup = 'edrnsite.collaborations.interfaces.collaborativegroupindex.ICollaborativeGroupIndex'
 
 class CommitteeFolderIngestor(KnowledgeFolderIngestor):
     '''Committee Folder ingestion.'''
+    def __call__(self):
+        renderResults = super(CommitteeFolderIngestor, self).__call__()
+        self.updateCollaborativeGroups()
+        return renderResults
+    def updateCollaborativeGroups(self):
+        '''Update members of any matching collaborative groups'''
+        catalog = getToolByName(aq_inner(self.context), 'portal_catalog')
+        for committee in [i.obj for i in self.objects if i.obj.committeeType == 'Collaborative Group']:
+            for collabGroup in [i.getObject() for i in catalog(object_provides=_collabGroup, Title=committee.title)]:
+                members = []
+                members.extend(committee.member)
+                members.extend(committee.consultant)
+                members.extend(committee.coChair)
+                members.extend(committee.chair)
+                collabGroup.setMembers(members)
 
 class CommitteeHandler(IngestHandler):
     '''Handler for ``Committee`` objects.'''
