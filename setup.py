0b708e838dfe5ca012586ffa94867cf64a0ace95
Sean Kelly <sean.c.kelly@me.com>
2011-07-27 03:13:18 +0000

    boosty

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ import os.path
 # ------------
 
 _name        = 'eke.committees'
-_version     = '1.0.4'
+_version     = '1.0.5'
 _description = 'EDRN Committee list and membership information for the EDRN Knowledge Environment'
 _url         = 'http://cancer.jpl.nasa.gov/products/eke-committees'
 _downloadURL = 'http://oodt.jpl.nasa.gov/dist/eke'
